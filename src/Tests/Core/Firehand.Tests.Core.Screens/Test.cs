fd59955a46d3bdbefec1e393cbd627c982ac29f9
Mário Quirino <mario.mars.05@gmail.com>
2013-05-13 00:40:50 +0100

    Solution and projects creation.

---
+++ src/Tests/Core/Firehand.Tests.Core.Screens/Test.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System;
+
+namespace Firehand.Tests.Core.Screens
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase( )
+		{
+		}
+	}
+}
+
---
+++ src/Tests/Core/Firehand.Tests.Core.Tongues/Test.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System;
+
+namespace Firehand.Tests.Core.Tongues
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase( )
+		{
+		}
+	}
+}
+
